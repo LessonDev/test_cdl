5f3f5f89ec62ed9fd1abbfdb6094c250f187b52c
LessonDev <fl.lesson.dev@gmail.com>
2021-02-16 14:02:43 +0100

    Mise en place de formulaire pour création des catégories

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/categorie", name="categorie")
+     * Permet crée une catégorie
+     * @Route("/editcategory", name="editcategory")
+     * @return Response
      */
-    public function index(): Response
-    {
+    public function editCategory(
+        Request $request,
+        EntityManagerInterface $manager
+    ) {
+        $categorie = new Categorie();
+
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($categorie);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'form' => $form->createView(),
         ]);
     }
 }
