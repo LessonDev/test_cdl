2e11ad136572c058b8cf4df0933e0fc813df73d0
LessonDev <fl.lesson.dev@gmail.com>
2021-02-16 14:02:43 +0100

    Mise en place de formulaire pour création des catégories

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -6,14 +6,18 @@ use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategorieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('topic')
-        ;
+        $builder->add('Topic', TextType::class, [
+            'required' => true,
+            'attr' => [
+                'placeholder' => 'Entre nouvelle catégorie',
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
