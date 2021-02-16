41af3bed36e2a3008ef2e06e747f9a9373defa5a
LessonDev <fl.lesson.dev@gmail.com>
2021-02-16 14:19:53 +0100

    Mise en place de formulaire pour création des auteurs

--- src/Controller/AuteurController.php
+++ src/Controller/AuteurController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Auteur;
+use App\Form\AuteurType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AuteurController extends AbstractController
 {
     /**
-     * @Route("/auteur", name="auteur")
+     * Permet créer une auteur
+     * @Route("/editauthor", name="editauthor")
+     * @return Response
      */
-    public function index(): Response
-    {
+    public function editAuteur(
+        Request $request,
+        EntityManagerInterface $manager
+    ) {
+        $auteur = new Auteur();
+
+        $form = $this->createForm(AuteurType::class, $auteur);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($auteur);
+            $manager->flush();
+
+            return $this->redirectToRoute('home');
+        }
         return $this->render('auteur/index.html.twig', [
-            'controller_name' => 'AuteurController',
+            'form' => $form->createView(),
         ]);
     }
 }
