75f85fb002a24066a3fe72b0f8e0fb6327c8f73c
LessonDev <fl.lesson.dev@gmail.com>
2021-02-16 14:19:53 +0100

    Mise en place de formulaire pour création des auteurs

--- src/Form/AuteurType.php
+++ src/Form/AuteurType.php
@@ -6,15 +6,29 @@ use App\Entity\Auteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AuteurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('birth')
-        ;
+            ->add('name', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Entrer un nouvel Auteur',
+                ],
+            ])
+            ->add('birth', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'required' => false,
+                'attr' => [
+                    'placeholder' =>
+                        'Renseigner date de naissance d\'auteur dd-mm-yyyy ',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
